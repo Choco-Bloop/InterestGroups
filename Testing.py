75e4d4d0498ee1a8d4664e83fcbece97dcf651ba
Choco-Bloop <152234746+Choco-Bloop@users.noreply.github.com>
2025-05-11 02:28:44 +0800

    Add files via upload

---
+++ Testing.py
@@ -0,0 +1,5 @@
+# This is a file that serves as a way to reach the IDLE Shell to test
+# sqlite commands as Crtl+C sometimes doesn't work and restarting shell 
+# requires me to chdir to the correct folder again
+
+print('testing')
